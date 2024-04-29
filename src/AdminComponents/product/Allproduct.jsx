c6c892227ede839ba495af2768e013412713a15e
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-04-29 09:39:35 +0530

    update UI

--- src/AdminComponents/product/Allproduct.jsx
+++ src/AdminComponents/product/Allproduct.jsx
@@ -14,7 +14,7 @@ const Allproduct = () => {
   const [categories, setCategories] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [stockQuery, setStockQuery] = useState("");
-  const [ResultPerPage, setResultPerPage] = useState(20);
+  const [ResultPerPage, setResultPerPage] = useState(30);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [toalProduct, setToalProduct] = useState(0);
@@ -24,9 +24,7 @@ const Allproduct = () => {
       try {
         setLoading(true);
         const response = await makeApi(
-          `/api/get-all-products?name=${searchQuery}&
-          category=${category}&&IsOutOfStock=${stockQuery}
-          &page=${currentPage}&perPage=${ResultPerPage}`,
+          `/api/get-all-products?name=${searchQuery}&IsOutOfStock=${stockQuery}&page=${currentPage}&perPage=${ResultPerPage}&category=${category}`,
           "GET"
         );
         setProducts(response.data.products);
@@ -99,14 +97,14 @@ const Allproduct = () => {
           {/* search */}
           <div>
             <div className="inputBox_container">
-              <svg
+              {/* <svg
                 className="search_icon"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 48 48"
                 alt="search icon"
               >
                 <path d="M46.599 46.599a4.498 4.498 0 0 1-6.363 0l-7.941-7.941C29.028 40.749 25.167 42 21 42 9.402 42 0 32.598 0 21S9.402 0 21 0s21 9.402 21 21c0 4.167-1.251 8.028-3.342 11.295l7.941 7.941a4.498 4.498 0 0 1 0 6.363zM21 6C12.717 6 6 12.714 6 21s6.717 15 15 15c8.286 0 15-6.714 15-15S29.286 6 21 6z"></path>
-              </svg>
+              </svg> */}
               <input
                 className="inputBox"
                 id="inputBox"
